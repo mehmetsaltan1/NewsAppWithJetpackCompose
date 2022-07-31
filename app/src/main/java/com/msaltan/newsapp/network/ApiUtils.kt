2cd938558796ceac95cd6f152c160b68f44a9769
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-07-31 13:08:15 +0300

    change endpoint

--- app/src/main/java/com/msaltan/newsapp/network/ApiUtils.kt
+++ app/src/main/java/com/msaltan/newsapp/network/ApiUtils.kt
@@ -3,7 +3,7 @@ package com.msaltan.newsapp.network
 class ApiUtils {
     companion object {
         private const val BASE_URL = "https://newsapi.org/"
-        const val endPoint = "v2/top-headlines?country=tr&apiKey=7e2876177804465db0b898b6b5f0cd94"
+        const val endPoint = "v2/top-headlines?sources=bbc-news&apiKey=7e2876177804465db0b898b6b5f0cd94"
         fun getNewsDao(): NewsDao {
             return RetrofitClient.getClient(BASE_URL).create(NewsDao::class.java)
         }
